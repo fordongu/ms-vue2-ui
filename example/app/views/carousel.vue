0869c9f59321b72834aabd0c7155eda8b81c57aa
Bane.Shi <fengxiaotx@163.com>
2017-01-14 23:58:38 +0800

    [Bane.Shi]

---
+++ example/app/views/carousel.vue
@@ -0,0 +1,18 @@
+<!--
+Created by Bane.Shi.
+Copyright MoenSun
+User: Bane.Shi
+Date: 2017/1/14
+Time: 23:53-->
+<template>
+
+</template>
+<script>
+    export default {
+      components: {
+      }
+    }
+</script>
+<style>
+
+</style>
---
+++ src/packages/carousel/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Bane.Shi.
+ * Copyright MoenSun
+ * User: Bane.Shi
+ * Date: 2017/1/14
+ * Time: 23:54
+ */
+'use strict';
