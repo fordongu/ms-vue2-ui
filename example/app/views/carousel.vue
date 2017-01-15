3c0e633ec2ece30cd304b7d0e9dd362c0d39f221
fengx <fengxiaotx@163.com>
2017-01-15 12:39:21 +0800

    Merge branch 'feature/carousel' into develop
    # Conflicts:
    #	example/app/views/carousel.vue
    #	src/packages/grid/GridEvents.js
    #	src/packages/theme-default/style.scss

--- example/app/views/carousel.vue
+++ example/app/views/carousel.vue
@@ -5,44 +5,7 @@ User: Bane.Shi
 Date: 2017/1/14
 Time: 23:53-->
 <template>
-<<<<<<< HEAD
-<<<<<<< HEAD:example/app/views/carousel.vue
-
-=======
-  <div>
-    <div></div>
-    <div>
-      <ms-grid-body :tree-structure="treeStructure" :data="data" :columns="columns"  />
-    </div>
-  </div>
->>>>>>> feature/grid:src/packages/grid/grid-center.vue
-</template>
-<script>
-    import PropsMixin from "./mixins/PropsMixin";
-    import MethodsMixin from "./mixins/MethodsMixin";
-
-    import MsGridBody from "./grid-body.vue";
-    export default {
-<<<<<<< HEAD:example/app/views/carousel.vue
-=======
-      name:'ms-grid-center',
-      mixins:[PropsMixin,MethodsMixin],
-      props:{
-        centerColumns:{
-          type:Array,
-          default:function() {
-            return [];
-          }
-        }
-      },
-      mounted(){
-        debugger
-      },
->>>>>>> feature/grid:src/packages/grid/grid-center.vue
-      components: {
-        MsGridBody
-=======
-<div class="carousel">
+  <div class="carousel">
     <carousel>
 
     </carousel>
@@ -66,7 +29,6 @@ Time: 23:53-->
         },
       components: {
         Carousel
->>>>>>> feature/carousel
       }
     }
 </script>
