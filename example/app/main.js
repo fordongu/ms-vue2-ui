0d2c0b890efd3963ec2b6ace0604ed02be813604
fengx <fengxiaotx@163.com>
2017-01-15 12:11:16 +0800

    [Bane.Shi]

--- example/app/main.js
+++ example/app/main.js
@@ -17,14 +17,6 @@ import routers from "./routes/route";
 console.log(routers);
 Vue.use(VueRouter);
 
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-const routes = [
-    { path: '/',component: require("./views/index.vue")},
-    { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
-]
 
 const router = new VueRouter({
     routes:routers
