095f9e549ecd35a3677b0dfeb05f397d17734c74
fengx <fengxiaotx@163.com>
2017-01-15 12:22:26 +0800

    [Bane.Shi]

--- src/packages/carousel/carousel.vue
+++ src/packages/carousel/carousel.vue
@@ -2,41 +2,26 @@
 Created by Bane.Shi.
 Copyright MoenSun
 User: Bane.Shi
-Date: 2017/1/14
-Time: 23:58-->
+Date: 2017/1/15
+Time: 12:20-->
 <template>
-  <div class="ms-carousel carousel slide">
-    <ol class="carousel-indicators">
-
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <slot></slot>
+    <div>
+ssss
     </div>
-    <a class="left carousel-control" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
 </template>
 <script>
-    export default {
-      name:'ms-carousel',
-      props:{
-        data:{
-          type:Array,
-          default(){
-            return [];
-          }
+
+    export default{
+        data(){
+            return{
+            }
+        },
+        components:{
+
         }
-      },
-      components: {
-      }
     }
 </script>
 <style>
 
 </style>
+
