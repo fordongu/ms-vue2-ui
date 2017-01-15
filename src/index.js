9af7a0da9a0a19472de5147661fbcf9acded4475
fengx <fengxiaotx@163.com>
2017-01-15 12:51:47 +0800

    [Bane.Shi]

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Bane.Shi.
+ * Copyright MoenSun
+ * User: Bane.Shi
+ * Date: 2017/1/15
+ * Time: 12:40
+ */
+'use strict';
+import {MsGrid} from "./packages/grid/index";
+
+module.exports = {
+    MsGrid
+}
---
+++ src/packages/grid/grid-center.vue
@@ -0,0 +1,34 @@
+<!--
+Created by Bane.Shi.
+Copyright MoenSun
+User: Bane.Shi
+Date: 2017/1/15
+Time: 12:45-->
+<template>
+    <div>
+        <ms-grid-body :tree-structure="treeStructure"
+                      :data="data"
+                      :columns="columns">
+        </ms-grid-body>
+    </div>
+</template>
+<script>
+    import PropsMixin from "./mixins/PropsMixin";
+    import MethodsMixin from "./mixins/MethodsMixin";
+
+    import MsGridBody from "./grid-body.vue";
+    export default{
+        mixins:[PropsMixin,MethodsMixin],
+        data(){
+            return{
+            }
+        },
+        components:{
+            MsGridBody
+        }
+    }
+</script>
+<style>
+
+</style>
+
