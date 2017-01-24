ce14b4f848e3504efb574b1e3ea083240665bf9e
bane <fengxiaotx@163.com>
2017-01-24 22:23:04 +0800

    [Bane.Shi]

--- example/app/views/layout.vue
+++ example/app/views/layout.vue
@@ -7,10 +7,11 @@ Date: 2017/1/23
 Time: 23:51-->
 <template>
   <layout>
-    <h1 slot="header">这里可能是一个页面标题</h1>
+    <h1 slot="north">这里可能是一个页面标题</h1>
+    <div slot="east">实打实的</div>
     <p>主要内容的一个段落。</p>
     <p>另一个主要段落。</p>
-    <p slot="footer">这里有一些联系信息</p>
+    <p slot="south">这里有一些联系信息</p>
   </layout>
 </template>
 <script>
